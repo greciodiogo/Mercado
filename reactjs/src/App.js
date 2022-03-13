a462a5ca3fbdfbf6b3f09158ee3dc0918501f2b9
greciodiogo <fonebahia8@gmail.com>
2022-03-13 07:54:40 +0100

    The Routes were created and added to App.js

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-function App() {
-  return (
-   <div className='app'>
-     <h1>Projecto inicializado</h1>
-   </div>
+import {commerce} from './lib/commerce';
+import {Products, Navbar} from './components'
+import {BrowserRouter,Routes,Route } from 'react-router-dom'
 
+const App =() =>{
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] =useState({});
+  
+  const fetchProducts = async () => {
+    const {data} = await commerce.products.list();
+  
+    setProducts(data);
+  }
+  const fetchCart = async() =>{
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async (productId, quantity) =>{
+    const item = await commerce.cart.add(productId, quantity)
+    setCart(item.cart)
+  }
+
+  useEffect(() => {
+    fetchProducts([]);
+  }, []);
+
+  useEffect(() => {
+    fetchCart([]);
+  }, []);
+
+
+  return (
+    <BrowserRouter> 
+      <div className='app'>
+        <Navbar totalItems={cart.total_items}/> 
+        <Routes>
+          <Route exact path='/' element={<Products/>}>
+              <Products products={products} onAddToCart={handleAddToCart}/>
+          </Route>
+          <Route exact path='/cart' element={<Cart/>}>
+              <Cart cart={cart} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
