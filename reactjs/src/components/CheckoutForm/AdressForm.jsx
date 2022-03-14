55c99eabe02326f2ef2971e78099ebbe717c65e3
greciodiogo <fonebahia8@gmail.com>
2022-03-14 22:53:35 +0100

    Added the Link to the component

--- reactjs/src/components/CheckoutForm/AdressForm.jsx
+++ reactjs/src/components/CheckoutForm/AdressForm.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import {InputLabel, Select, MenuItem, Button, Grid, Typography} from '@material-ui/core'
-import {useFrom, FormProvider} from 'rect-hook-form'
+import {Button, Grid, Typography} from '@material-ui/core'
+import {useForm, FormProvider} from 'react-hook-form'
 import FormInput from './CustomTextField'
-import { Link } from 'react-router-dom'
-
 const AdressForm = ({next}) => {
     const methods = useForm()
     return (
-    <>
+    <>0
         <Typography variant="h6" gutterBottom>Endereço de Entrega</Typography>
         <FormProvider {...methods}>
             <form onSubmit={methods.handleSubmit((data) => next(...data) )}>
                 <Grid container spacing={3}>
-                    <FormInpput required name='firstName' label='Primeiro Nome'/>
-                    <FormInpput required name='lastName' label='Ultimo Nome'/>
-                    <FormInpput required name='address1' label='Address'/>
-                    <FormInpput required name='email' label='Email'/>
-                    <FormInpput required name='morada' label='morada'/>
+                    <FormInput required name='firstName' label='Primeiro Nome'/>
+                    <FormInput required name='lastName' label='Ultimo Nome'/>
+                    <FormInput required name='address1' label='Address'/>
+                    <FormInput required name='email' label='Email'/>
+                    <FormInput required name='morada' label='morada'/>
                 </Grid>
             </form>
             <br/>
             <div style={{display:"flex", justifyContent:"space-between" }}>
-                <Button component={Link} variant='outlined'>Voltar</Button>
+                <Button variant='outlined'>Voltar</Button>
                 <Button type='submit' variant='contained' color='primary'>Avançar</Button>
             </div>
         </FormProvider>
