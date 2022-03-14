a6814e56629cea38eac13824b32f80d2b51ce870
greciodiogo <fonebahia8@gmail.com>
2022-03-14 22:58:53 +0100

    I don't know why but I am with a problem with this component

--- reactjs/src/components/CheckoutForm/CustomTextField.jsx
+++ reactjs/src/components/CheckoutForm/CustomTextField.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { TextField, Grid } from '@material-ui/core'
 import { useFormContext, Controller } from 'react-hook-form'
 
-const FormInpput = ({name,label,required}) => {
+const FormInput = ({name,label}) => {
   const {control} = useFormContext()
-    return (
+
+  return (
     <Grid item xs={12} sm={6}>
       <Controller as={TextField} control={control} name={name} label={label} required />
     </Grid >
   )
 }
 
-export default FormInpput
+export default FormInput
